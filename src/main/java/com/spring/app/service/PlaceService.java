f51db330e271ead0bb284588f70d2faca6da903e
dao00 <dao00@LAPTOP-KP6K5H6C>
2021-04-09 14:54:03 +0900

    폴더,파일,페이지 구성.

---
+++ src/main/java/com/spring/app/service/PlaceService.java
@@ -0,0 +1,38 @@
+package com.spring.app.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.app.domain.PlaceDAO;
+import com.spring.app.domain.PlaceDTO;
+
+@Service
+public class PlaceService {
+
+	PlaceDAO dao;
+	
+	// MyBatis
+	private SqlSession sqlSession;
+	
+	@Autowired
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	
+	public List<PlaceDTO> list(){
+		dao = sqlSession.getMapper(PlaceDAO.class);
+		return dao.select();
+	}
+	
+	public List<PlaceDTO> viewByUid(int contentId){
+		dao = sqlSession.getMapper(PlaceDAO.class);
+		
+		dao.incViewCnt(contentId);
+		
+		return dao.selectByUid(contentId);
+	}
+	
+}
