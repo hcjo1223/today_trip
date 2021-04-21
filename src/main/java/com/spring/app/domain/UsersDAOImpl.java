cbfdc2c2618f1330b5d526537187b8fb56001e01
kdd4867 <axav@naver.com>
2021-04-21 15:10:44 +0900

    유저 권한

--- src/main/java/com/spring/app/domain/UsersDAOImpl.java
+++ src/main/java/com/spring/app/domain/UsersDAOImpl.java
@@ -20,6 +20,7 @@ public class UsersDAOImpl implements UsersDAO{
 	@Override
 	public void register(UsersDTO usersDTO) throws Exception {
 		session.insert(NAMESPACE + ".register",usersDTO);
+		session.insert(NAMESPACE + ".registerAuthority", usersDTO);
 	}
 	
 	
---
+++ src/main/java/com/spring/app/domain/UsersProfileDAO.java
@@ -0,0 +1,15 @@
+package com.spring.app.domain;
+
+import java.util.List;
+
+public interface UsersProfileDAO {
+
+	List<CalenderDTO> selectUserCalender();
+	
+	List<CardsDTO> selectUserCards();
+	
+	List<UsersDTO> selectUsersInfo();
+	
+	
+	
+}
