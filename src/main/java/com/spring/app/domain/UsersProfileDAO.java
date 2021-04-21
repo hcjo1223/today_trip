045f8a57d0819457390fb7c3276c2a9c694b0670
axav <axav@DESKTOP-R9JJV1S>
2021-04-21 16:13:10 +0900

    111

--- src/main/java/com/spring/app/domain/UsersProfileDAO.java
+++ src/main/java/com/spring/app/domain/UsersProfileDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface UsersProfileDAO {
 
-	List<CalendarDTO> selectUserCalender();
+	List<CalenderDTO> selectUserCalender();
 	
 	List<CardsDTO> selectUserCards();
 	
